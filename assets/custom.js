854ca6cfd8734baa45d81dbbd10cdb3518caeb7f
leynau <leynau11@gmail.com>
2024-09-26 22:29:39 +0300

    shopify product slide

---
+++ assets/custom.js
@@ -0,0 +1,51 @@
+const slider = document.getElementById('carousel');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+    isDown = true;
+    slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('mouseup', () => {
+    isDown = false;
+    slider.classList.remove('active');
+});
+
+slider.addEventListener('mouseleave', () => {
+    isDown = false;
+    slider.classList.remove('active');
+});
+
+slider.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const SCROLL_SPEED = 1;
+    const walk = (x - startX) * SCROLL_SPEED;
+    slider.scrollLeft = scrollLeft - walk;
+});
+
+slider.addEventListener('touchstart', (e) => {
+    isDown = true;
+    slider.classList.add('active');
+    startX = e.touches[0].pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('touchend', () => {
+    isDown = false;
+    slider.classList.remove('active');
+});
+
+slider.addEventListener('touchmove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.touches[0].pageX - slider.offsetLeft;
+    const SCROLL_SPEED = 1;
+    const walk = (x - startX) * SCROLL_SPEED;
+    slider.scrollLeft = scrollLeft - walk;
+});
---
+++ tailwind.config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  content: [
+    './**/*.html',
+    './**/*.liquid',
+    './**/*.js',
+    './**/*.vue',
+    '!./node_modules/**',
+  ],
+  theme: {
+    extend: {
+      margin: {
+        '5': '2rem',
+        '10': '5rem',
+      },
+      padding: {
+        '15': '15rem',
+      },
+      maxWidth: {
+        'sm': '18rem',
+      },
+      fontFamily: {
+        custom: ['"ITC Avant Garde Gothic Pro"', 'sans-serif'],
+        body: ['Avenir', 'sans-serif'],
+      },
+      fontSize: {
+        'custom-size': '31px',
+        'button-size': '12px',
+        'title-size': '20px',
+      },
+      lineHeight: {
+        'custom-line-height': '40px',
+        'button-line-height': '17.48px',
+        'title-line-height': '26px',
+        body: '25.6px',
+      },
+      fontWeight: {
+        'custom-weight': 700,
+        'button-weight': 900,
+      },
+      colors: {
+        'custom-blue': '#99C3CC',
+        'custom-red': '#CC9999',
+        'custom-purple': '#CB99CC',
+        'custom-green': '#BCD9B2',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
